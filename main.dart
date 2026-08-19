deb1f0d93acaf9b3e49aa535ea06c9c7fb93bad3
almareinoso6 <almareinoso6@gmail.com>
2026-08-19 20:25:01 -0300

    Agendamdz main.dart
    Updated project details and descriptions in main.dart.

--- main.dart
+++ main.dart
@@ -1,17 +1,11 @@
-// main.dart
-// Trabajo Práctico N°1 - Desarrollo de Aplicaciones Web
-// UCA - 2026
-
 void main() {
-  print("Integrantes: $integrante1 y $integrante2");
-  print("Carrera: $carrera");
-  print("Materia: $materia");
-  print("Proyecto: $proyecto");
-  print("Descripción: $descripcion");
-  print("Funcionalidades:");
+  print("Integrantes: Alma Reinoso y Manuela Fernandez");
+  print("Carrera: Comunicacion Digital");
+  print("Materia: Desarrollo de Desarrollo de Aplicaciones Web");
+  print("Proyecto: AgendaMDZ");
+  print("Descripción: Plataforma digital diseñada para centralizar eventos, actividades y experiencias de Mendoza en un único espacio moderno, visual e intuitivo");
   print("- Descubrir eventos");
   print("- Filtrar por intereses");
   print("- Ver actividades gratuitas y pagas");
   print("- Organizar planes desde el celular");
-  print("Tecnologías: Dart, Flutter, Firebase");
 }
